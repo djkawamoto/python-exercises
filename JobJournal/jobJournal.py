0a533615621b5c426cced4a26f0e653993aec422
Daniel Kawamoto <djkawamoto@gmail.com>
2016-04-27 22:24:22 -0700

    Adding the most complete version of jobJournal.py

--- JobJournal/jobJournal.py
+++ JobJournal/jobJournal.py
@@ -1,82 +1,138 @@
 #!/Library/Frameworks/Python.framework/Versions/3.4/bin/python3
 # jobJournal.py
 
-import tkinter
+import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox as mb
-# import os as os
-# import datetime
-# import csv
-
-'''
-Don't forget to set the default values of the comboboxes!!!
-Rewrite to include proper use of classes and separate GUI setup from init function.
-check the linda example using pack and grid together.
-'''
+import time as t
+import csv
 
 
 class MainApp:
 
     def __init__(self, master):
+
         master.title("DAILY JOB JOURNAL")
         self.style = ttk.Style()
-
-        # self.frame_content = ttk.Frame(master)
-        # self.frame_content.grid()
+        master.configure(background='#ececec', padx=30, pady=20)
+        master.resizable(False, False)
 
         # job label
-        ttk.Label(master, text="Job #:").grid(row=0, column=0, sticky="W")
+        ttk.Label(master, text="Job #:").grid(
+            row=0, column=0, padx=10, pady=10, sticky="W")
         # job text entry box
         self.job = ttk.Entry(master)
-        self.job.grid(row=1, column=0)
+        self.job.grid(row=1, column=0, padx=10, sticky="W")
 
         # project label
-        ttk.Label(master, text="Project:").grid(row=0, column=1, sticky="W")
+        ttk.Label(master, text="Project:").grid(
+            row=0, column=1, padx=10, pady=10, sticky="W")
         # project text entry box
         self.project = ttk.Entry(master)
-        self.project.grid(row=1, column=1, columnspan=2)
+        self.project.grid(row=1, column=1, padx=10, sticky="W")
 
-        # start label
-        ttk.Label(master, text="Start").grid(row=2, column=0, sticky="W")
-        # start combobox
-        self.start = ttk.Combobox(master)
-        self.start.grid(row=3, column=0)
+        # date label
+        ttk.Label(master, text="Date (MM/DD/YY):").grid(
+            row=2, column=0, padx=10, pady=10, sticky="W")
+        # date entry
+        self.date = ttk.Entry(master)
+        self.date.grid(row=3, column=0, padx=10, sticky="W")
 
-        # stop label
-        ttk.Label(master, text="Stop").grid(row=2, column=1, sticky="W")
-        # stop combobox
-        self.stop = ttk.Combobox(master)
-        self.stop.grid(row=3, column=1)
+        # hours label
+        ttk.Label(master, text="Hours:").grid(
+            row=2, column=1, padx=10, pady=10, sticky="W")
+        # hours entry
+        self.hours = ttk.Entry(master)
+        # self.hours.set("ie. 1.5")
+        self.hours.grid(row=3, column=1, padx=10, sticky="W")
 
         # notes label
-        ttk.Label(master, text="Notes:").grid(row=4, column=0, sticky="W")
+        ttk.Label(master, text="Notes:").grid(
+            row=5, column=0, padx=10, pady=10, sticky="W")
         # notes text entry box
-        self.notes = ttk.Entry(master)
-        # , rowspan = 5, columnspan = 3)
-        self.notes.grid()#row=5, column=0, rowspan=5, columnspan=3 sd)
+        self.notes = tk.Text(master, width=75, height=10)
+        self.notes.config(borderwidth=1)
+        self.notes.grid(row=6, column=0, columnspan=2, padx=10,
+                        ipadx=5, ipady=5, sticky="W")
 
         # submit button
-        submit_button = ttk.Button(master, text="Submit").grid(row=3, column=2)
-        # submit_button.grid(master, row=3, column=2) #, text="Submit"
+        self.submit_button = ttk.Button(
+            master, text="Submit", command=self.checkJob)
+        self.submit_button.grid(
+            row=7, column=1, padx=10, pady=10, sticky="W")
+
+        # clear button
+        self.clear_button = ttk.Button(
+            master, text="Clear", command=self.clear)
+        self.clear_button.grid(row=7, column=1, padx=10, pady=10, sticky="E")
+
+    def checkJob(self):
+        jobLen = len(self.job.get())
+        if jobLen > 0:
+            self.checkProject()
+        else:
+            mb.showinfo(title="Whoops!", message="Please enter a job number.")
+
+    def checkProject(self):
+        projLen = len(self.project.get())
+        if projLen > 0:
+            self.checkDate()
+        else:
+            mb.showinfo(title="Whoops!",
+                        message="Please enter information about the project")
+
+    def checkDate(self):
+        try:
+            t.strptime(self.date.get(), "%m/%d/%y")
+            self.checkHours()
+        except ValueError:
+            mb.showinfo(title="Whoops!",
+                        message="Please enter date as 'MM/DD/YY'.")
+
+    def checkHours(self):
+        try:
+            float(self.hours.get())
+            self.checkNotes()
+        except ValueError:
+            mb.showinfo(
+                title="Whoops!", message="Please enter hours as a decimal (For example: '1.5'")
+
+    def checkNotes(self):
+        if len(self.notes.get(1.0, "end")) > 1:
+            self.submit()
+        else:
+            mb.askquestion(
+                title="Whoops!", message="You haven't entered any notes. Would you like to submit your job journal anyway?")
+            return False
 
     def submit(self):
-        # write to CSV
-        # clear form
-        date_submitted = 'THE DATE NEEDS TO BE CHANGED'  # Get value of date combobox
-        mb.showinfo(title='Success!', message='Your entry for ' +
-                    date_submitted + ' has been submitted.')
+        csvPath = "/Users/danielkawamoto/Documents/TTA/Old Courses/Python/Drills/JobJournal/JobJournal.csv"
+        job = self.job.get()
+        project = self.project.get()
+        date = self.date.get()
+        hours = self.hours.get()
+        notes = self.notes.get(1.0, "end")
+        with open(csvPath, 'a') as csvfile:
+            fieldnames = ['JOB', 'PROJECT', 'DATE', 'HOURS', 'NOTES']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writerow({'JOB': job, 'PROJECT': project,
+                             'DATE': date, 'HOURS': hours, 'NOTES': notes})
+        mb.showinfo(title="Success!", message="You've submitted:\n{}hours for '{}' on {}.".format(
+            self.hours.get(), self.project.get(), self.date.get()))
+        self.clear()
 
     def clear(self):
-        self.notes.delete(1.0, 'end')
+        self.job.delete(0, "end")
+        self.project.delete(0, "end")
+        self.date.delete(0, "end")
+        self.hours.delete(0, "end")
+        self.notes.delete(1.0, "end")
 
 
 def main():
-    root = tkinter.Tk()
+    root = tk.Tk()
     MainApp(root)
     root.mainloop()
 
 if __name__ == '__main__':
     main()
-    # root = Tk()
-    # mainapp = MainApp(root)
-    # root.mainloop()
